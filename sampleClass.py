32ba277eea835fec64db51411c6cde7048fd0bf1
Stephen-Jones82 <sjones8270ss@gmail.com>
2024-11-03 19:10:56 -0800

    Add files via upload

--- sampleClass.py
+++ sampleClass.py
@@ -1,19 +1,20 @@
 class PartyAnimal:
 
     def __init__(self):
-        self.x = 0
+      self.x = 0
 
-    def party(self):
+    def party(self) :
         self.x = self.x + 1
-        print("So far...", self.x)
-
+        print("So far",self.x)
 
 an = PartyAnimal()
 
-print("type is: ", type(an))
-print("dir is: ", dir(an))
-print("type of an.x is: ", type(an.x))
+print("Type", type(an))
+print("Dir ", dir(an))
+print("Type", type(an.x))
+print("Type", type(an.party))
 
-an.party()
-an.party()
-an.party()
+
+
+
+
